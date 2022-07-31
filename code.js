faaec6b3f410c9fb27fc46c02f50555a7a1ad1a5
Dario Cruz <rio2012@gmail.com>
2022-07-31 09:18:04 -0400

    Add js code for sketch functionality

--- code.js
+++ code.js
@@ -1,15 +1,19 @@
 // Defining html elements. 
 const gridContainer = document.getElementById('gridcontainer');
 const gridSelect = document.getElementById('GridSelect');
-// Defining elements to be created.
 
+// Defining elements to be created.
 const createGridBox = document.createElement('div');
+
 // Adding class for changing color later.
 createGridBox.setAttribute('id', 'pixel');
-
+createGridBox.setAttribute('class', 'gridpixels');
 // Create grid function with number arg. 
 function fillGrid(num) {
     for (let i = 0; i < num; i++) {
+        // Adds numbered pixel ID to generated divs
+        // createGridBox.setAttribute('id', `pixel-${i}`);
+        
         // cloneNode allows for cloning of the same item.
         // Without it this line would only create one grid item. 
         gridContainer.appendChild(createGridBox.cloneNode(true));
@@ -20,11 +24,17 @@ function fillGrid(num) {
     gridContainer.style.gridTemplateRows = `repeat(${Math.sqrt(num)}, 1fr)`;
 }
 
-fillGrid(256);
 // Defining const for elements created by the createGridBox.
 const gridPixel = document.getElementById('pixel');
 
+fillGrid(256);
+
 // Setting up even listeners for hovering. 
-gridPixel.addEventListener('mouseover', () => {
-    gridPixel.style.backgroundColor = 'black';
-})
+// Used a loop to apply even to multiple items.
+document.querySelectorAll('.gridpixels').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        // Sets the css for each itme on event. 
+        item.style.backgroundColor = 'black';
+    
+    })  
+});
